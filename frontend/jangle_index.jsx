481591830d51d7ac92c7aa9d8d0dda494fb087c9
Jeff <jeffreyfang123@gmail.com>
2018-10-19 12:12:59 +0900

    fix index

--- frontend/jangle_index.jsx
+++ frontend/jangle_index.jsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
  
   let store;
