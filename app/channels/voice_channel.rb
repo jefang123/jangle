641c5aee19e67f77b4667faa4c81def54d789454
Jeff <jeffreyfang123@gmail.com>
2018-10-26 03:30:52 +0900

    pseudo code

--- app/channels/voice_channel.rb
+++ app/channels/voice_channel.rb
@@ -8,3 +8,10 @@ class VoiceChannel < ApplicationCable::Channel
 end 
 
 # getusermedia API for microphone
+
+# pseudo-code
+
+# set up microphone access via voice channels 
+# set up mute/on for voice channels 
+# set up actioncable streaming for voice channels in servers 
+  # users should be able to listen regardless if the url is or isnt the voice channel
