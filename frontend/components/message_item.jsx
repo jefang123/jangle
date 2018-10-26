e33f7d540930c44dc3bd688978b07da44391daed
Jeff <jeffreyfang123@gmail.com>
2018-10-27 03:53:18 +0900

    Continue on Message Item

--- frontend/components/message_item.jsx
+++ frontend/components/message_item.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Link, withRouter, matchPath } from 'react-router-dom';
 
 class MessageItem extends React.PureComponent {
   constructor (props) {
     super(props)
+    this.handleClick = this.handleClick.bind(this)
   }
 
   contextMenu (e) {
@@ -16,35 +16,23 @@ class MessageItem extends React.PureComponent {
   }
 
   render () {
+    let messageb;
+    if (message.user_id === this.props.currentUser.id) {
+      messageb = <p className="delete-message" onClick={()=>this.handleClick(message.id)}>x</p>
+    }
+    return (
+      <div key ={message.id} className="message">
+        <img className="message-image" src={window.user_url}></img>
+        <div className="message-box">
+          <div>
+            <p>{user ? user.username : null} <span>{message.created_at}</span></p>
+            <p className="message-body">{message.body}</p>
+            {messageb}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default withRouter(MessageItem);
-
-
-// let messageb;
-// if (message.user_id === this.props.currentUser.id) {
-//   messageb = <p className="delete-message" onClick={()=>this.handleClick(message.id)}>x</p>
-// }
-
-// if (message.channel_id === this.props.channel.id) {
-//   const user = users[message.user_id];
-//   return (
-     
-//     <div key={message.id} className="message">
-//       <div>
-//         <img className="message-image" src={window.user_url}></img>
-//       <div className="message-box">
-//       <p >{user ? user.username : null} <span>{message.created_at}</span></p> 
-        
-
-//           <p className="message-body"> {message.body}</p>
-        
-//           {messageb}
-//       </div>
-//       </div>
-      
-//     </div>
-//   );
-// }
-// });
+export default MessageItem;
