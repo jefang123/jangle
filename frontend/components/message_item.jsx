f8765bbfae7534eafef79cbd05c2d4f6dc9b2abd
Jeff <jeffreyfang123@gmail.com>
2018-10-27 03:39:13 +0900

    Begin splitting code in message for a component

---
+++ frontend/components/message_item.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Link, withRouter, matchPath } from 'react-router-dom';
+
+class MessageItem extends React.PureComponent {
+  constructor (props) {
+    super(props)
+  }
+  contextMenu (e) {
+    e.preventDefault();
+  }
+
+  handleClick (e) {
+    e.preventDefault();
+  }
+
+  render () {
+    let match = matchPath(this.props.history.location.pathname, {
+      path: '/server/:serverId/',
+      exact: false,
+      strict: false 
+    });
+    let parameter;
+    if (!match) {
+      parameter = 0;
+    } else if (match.params.serverId) {
+      parameter = parseInt(match.params.serverId);
+    } else {
+      parameter = 0;
+    }
+
+    let klass;
+    
+    if (this.props.server.id === parameter) {
+      klass = "sv-selected"
+    } else {
+      klass = ""
+    }
+
+    let image;
+    if (!this.props.server.image_url) {
+      image = window.logo_url ;
+    } else {
+      image = this.props.server.image_url;
+    }
+    if (this.props.server.private) {
+      return (
+        <li
+          key={this.props.server.id}
+          className={klass}
+        > 
+          <div className="hidden">
+            <div className="arrow-left"></div>
+            <h3>{this.props.server.server_name}</h3>
+          </div>
+          <Link to={`/server/${this.props.server.id}/welcome`}>
+          <img src={image}></img>
+          </Link> 
+        </li>
+      );
+    } else {
+        return (
+          <li
+            key={this.props.server.id}
+            className={klass}
+          >
+            <div className="hidden">
+              <div className="arrow-left"></div>
+              <h3>{this.props.server.server_name}</h3>
+            </div>
+            <Link to={`/server/${this.props.server.id}`}>
+            <img src={image}></img>
+            </Link> 
+          </li>
+      );
+    }
+    
+  }
+}
+
+export default withRouter(MessageItem);
