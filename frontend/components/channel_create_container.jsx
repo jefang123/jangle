1f6122c5534bfbef938b74d4d3a9df740ebc20e8
Jeff <jeffreyfang123@gmail.com>
2018-10-30 09:13:03 +0900

    Remove unused code

--- frontend/components/channel_create_container.jsx
+++ frontend/components/channel_create_container.jsx
@@ -1,7 +1,6 @@
 import ChannelCreateForm from "./channel_create_form";
 import { connect } from 'react-redux';
 import { createChannel } from '../actions/channel_actions';
-import { fetchServer } from '../actions/server_actions';
 import { withRouter } from 'react-router-dom'
 
 const mapStateToProps = state => {
