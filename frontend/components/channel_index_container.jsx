d0f6ef661e2bbacfe759646d494e32752166e51a
Jeff <jeffreyfang123@gmail.com>
2018-10-31 11:16:08 +0900

    Work on channel files

--- frontend/components/channel_index_container.jsx
+++ frontend/components/channel_index_container.jsx
@@ -1,1 +1,17 @@
 import React from 'react';
+
+const mapStateToProps = state => {
+  return {
+    errors: state.errors.channel,
+    currentUser: state.entities.users[state.session.currentUserId],
+    users2: state.entities.users2
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    processForm: channel => dispatch(createChannel(channel))
+  };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ChannelCreateForm));
--- frontend/components/channel_index_item.jsx
+++ frontend/components/channel_index_item.jsx
@@ -5,25 +5,19 @@ if (channel.id === parameter) {
   klass = ""
 }
 
-let hash;
-let privateS = this.props.server.private;
 if (this.props.server.private) {
   hash = "@"
 } else {
   hash = "#"
 }
 
-let channelName;
-// if (privateS) {
   if (channel.channel_name === this.props.currentUser.username) {
     channelName = channel.channel_topic
   } 
-// }
 else {
   channelName = channel.channel_name
 }
 
-let channelb;
 if(this.props.server.creator_id === this.props.currentUser.id) {
   channelb = <Link to={`/server/${this.props.server.id}/channel/${channel.id}`}>
   <div className="delete-server" onClick={()=>{this.props.deleteChannel(channel.id)}}>
