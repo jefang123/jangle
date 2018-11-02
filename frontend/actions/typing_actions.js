1cca3895c87df60a021ff522dab794377c7a09c1
Jeff <jeffreyfang123@gmail.com>
2018-11-03 05:45:06 +0900

    Continue typing_actions

--- frontend/actions/typing_actions.js
+++ frontend/actions/typing_actions.js
@@ -1,16 +1,28 @@
 export const ADD_TYPER = 'ADD_TYPER';
 export const REMOVE_TYPER = 'REMOVE_TYPER'
 
-export const addTypingUser = typer => {
+const addTypingUser = typer => {
   return {
     type: ADD_TYPER,
     typer
   };
 };
 
-export const removeTypingUser = typer => {
+const removeTypingUser = typer => {
   return {
     type: REMOVE_TYPER,
     typer
   };
 };
+
+export const addTyper = typer => {
+  return dispatch => {
+    return dispatch(addTypingUser(typer));
+  }
+}
+
+export const removeTyper = typer => {
+  return dispatch => {
+    return dispatch(removeTypingUser(typer));
+  }
+}
--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -5,6 +5,7 @@ import channelsReducer from './channels_reducer';
 import messagesReducer from './messages_reducer';
 import privateUsersReducer from './private_users_reducer';
 import serverJoinsReducer from './serverjoins_reducer';
+import typingReducer from './typing_reducer';
 
 export default combineReducers({
   users2: privateUsersReducer,
@@ -12,5 +13,6 @@ export default combineReducers({
   servers: serversReducer,
   servers2: serverJoinsReducer,
   channels: channelsReducer,
+  typing: typingReducer,
   messages: messagesReducer
 });
