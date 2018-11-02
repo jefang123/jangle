c2aee448ee46fb7ae36efbd6470adc919648ef82
Jeff <jeffreyfang123@gmail.com>
2018-11-03 05:41:08 +0900

    Continue with typingUsers

--- frontend/reducers/typing_reducer.js
+++ frontend/reducers/typing_reducer.js
@@ -1,14 +1,15 @@
 import { ADD_TYPER, REMOVE_TYPER } from '../actions/typing_actions';
+import { merge } from 'lodash';
 
 export default (state={}, action) => {
   Object.freeze(state);
   switch (action.type) {
     case ADD_TYPER:
-      return {
-        currentUserId: action.currentUser.id
-      };
+      return merge({},state,{[action.message.id]:action.message})
     case REMOVE_TYPER:
-      return defaultState;
+      const newState = merge({}, state);
+      delete newState[action.messageId];
+      return newState;
     default:
       return state;
   }
