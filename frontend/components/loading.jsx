12be8d53f5abd12f5bbb05c4f5e7f82a50f60f5c
Jeff <jeffreyfang123@gmail.com>
2018-11-10 07:53:06 +0900

    Start working on loading

--- frontend/components/loading.jsx
+++ frontend/components/loading.jsx
@@ -2,8 +2,45 @@ import React from 'react';
 
 class Loading extends React.PureComponent {
   render () {
+    return (
+    <div className="server-show">
+        <section className='channel-index'>
+          <section className='server-heading'>
+            <h3>{server.server_name}</h3>
+            <br />
+          </section>
+          <ul>
+            {channels}
+          </ul>
+          <div className="user-tab">
+              <div>
+
+              <img src={window.logo_url}></img>
+              </div>
+              <section>
+                <p>{this.props.currentUser.username}</p>
+                <p># {this.props.currentUser.id}</p>
+              </section>
+              <button onClick={this.props.logout}>Log Out</button>
+          </div>
     
+        </section>
+        <section className='channel-show'>
+        <ProtectedRoute path='/server/:serverId/channel/:channelId' component={ChannelShowContainer} />
+        
+        </section>
+        <section className='user-index'>
+          <h3> Users </h3>
+          <div className="divider" />
+          <ul>
+
+          {users}
+          </ul>
+        </section>
+      </div>
+    )
   }
 
+}
 
 export default Loading;
