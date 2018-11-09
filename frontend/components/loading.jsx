b50bff4e03940791329b7b4751489aa656f13ed0
Jeff <jeffreyfang123@gmail.com>
2018-11-10 06:33:36 +0900

    Start on Loading Container

--- frontend/components/loading.jsx
+++ frontend/components/loading.jsx
@@ -2,15 +2,7 @@ import React from 'react';
 
 class Loading extends React.PureComponent {
   render () {
-    if (this.props.currentUser) {
-      return (
-          <ServerIndexContainer />
-      );} else {
-        return (
-          <Splash />
-        )
-      };
-    }
+    
   }
 
 
