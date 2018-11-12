d4c2bdd7cc0039d2929787f54cc6b705f05b8667
Jeff <jeffreyfang123@gmail.com>
2018-11-13 08:52:23 +0900

    Redo channel index jsx

--- frontend/components/channel_index_item.jsx
+++ frontend/components/channel_index_item.jsx
@@ -1,36 +1,5 @@
 import React from 'react';
-if (channel.id === parameter) {
-  klass = "ch-selected"
-} else {
-  klass = ""
-}
 
-if (this.props.server.private) {
-  hash = "@"
-} else {
-  hash = "#"
+class ChannelIndexItem {
+  
 }
-
-  if (channel.channel_name === this.props.currentUser.username) {
-    channelName = channel.channel_topic
-  } 
-else {
-  channelName = channel.channel_name
-}
-
-if(this.props.server.creator_id === this.props.currentUser.id) {
-  channelb = <Link to={`/server/${this.props.server.id}/channel/${channel.id}`}>
-  <div className="delete-server" onClick={()=>{this.props.deleteChannel(channel.id)}}>
-  X
-  <div className="delete-hidden"><div className="arrow-left"></div>Delete Channel </div>
-  </div>
-</Link>
-} 
-return (
-  <li key={channel.id} className={klass}>
-    <Link to={`/server/${this.props.server.id}/channel/${channel.id}`}>
-      {hash} {channelName} </Link>
-    {channelb}
-    
-  </li>
-)
