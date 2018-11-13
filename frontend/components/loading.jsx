fc39859581e1318dcc803480d343146adb2904e3
Jeff <jeffreyfang123@gmail.com>
2018-11-14 08:03:19 +0900

    Work on loading component

--- frontend/components/loading.jsx
+++ frontend/components/loading.jsx
@@ -4,38 +4,10 @@ class Loading extends React.PureComponent {
   render () {
     return (
     <div className="server-show">
-        <section className='channel-index'>
-          <section className='server-heading'>
-            <h3>{server.server_name}</h3>
-            <br />
-          </section>
-          <ul>
-            {channels}
-          </ul>
-          <div className="user-tab">
-              <div>
-
-              <img src={window.logo_url}></img>
-              </div>
-              <section>
-                <p>{this.props.currentUser.username}</p>
-                <p># {this.props.currentUser.id}</p>
-              </section>
-              <button onClick={this.props.logout}>Log Out</button>
-          </div>
-    
-        </section>
-        <section className='channel-show'>
-
-        </section>
-        <section className='user-index'>
-          <h3> Users </h3>
-          <div className="divider" />
-          <ul>
-
-          </ul>
-        </section>
-      </div>
+       <section className= "channel-index"></section>
+       <section className= "channel-show"></section>
+       <section className= "user-index"></section>
+    </div>
     )
   }
 
