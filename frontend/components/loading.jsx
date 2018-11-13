bd3d54fb4885d57fe1a5ba2816b8f1f178fd045e
Jeff <jeffreyfang123@gmail.com>
2018-11-14 08:07:19 +0900

    Work on loading component - serverheading

--- frontend/components/loading.jsx
+++ frontend/components/loading.jsx
@@ -4,7 +4,26 @@ class Loading extends React.PureComponent {
   render () {
     return (
     <div className="server-show">
-       <section className= "channel-index"></section>
+       <section className= "channel-index">
+        <section className= "server-heading">
+          <h3> </h3>
+        </section>
+        <ul>
+          <></>
+          <></>
+          <></>
+          <></>
+        </ul>
+        <div className= "user-tab">
+          <div>
+            <img src=""></img>
+          </div>
+          <section>
+            <p></p>
+            <p></p>
+          </section>
+        </div>
+       </section>
        <section className= "channel-show"></section>
        <section className= "user-index"></section>
     </div>
