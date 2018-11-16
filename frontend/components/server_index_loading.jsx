4d9cb3f752028bb6b20238dcd67f579f94b02c9b
Jeff <jeffreyfang123@gmail.com>
2018-11-17 03:04:44 +0900

    Add ServerLoad Component

---
+++ frontend/components/server_index_loading.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class ServerLoad extends React.PureComponent {
+  render() {
+    return(
+      <section className="server-index">
+        <ul>
+          <li>
+            <img></img>
+          </li>
+          <div className="divider"></div>
+          <li>
+            <img></img>
+          </li>
+          <li>
+            <img></img>
+          </li>
+          <li>
+            <img></img>
+          </li>
+          <li>
+            <img></img>
+          </li>
+          <li>
+            <button className="create-server" >+</button>
+          </li>
+        </ul>
+      </section>
+    );
+  }
+}
+
+export default ServerLoad
