45d7dd0b629e2e44831661487de10078d6651015
Jeff <jeffreyfang123@gmail.com>
2018-12-01 06:01:21 +0900

    Work on sorting message logic

--- frontend/components/channel_show.jsx
+++ frontend/components/channel_show.jsx
@@ -84,11 +84,32 @@ class ChannelShow extends React.PureComponent {
         currentUser={this.props.currentUser}
         deleteMessage={this.props.deleteMessage}
       />
-    // const filteredMessages = this.props.messages.filter(message => {
-    //   if (message.channel_id === this.props.channel.id) {
-    //     return message
-    //   }
-    // })
+    const filteredMessages = this.props.messages.filter(message => {
+      if (message.channel_id === this.props.channel.id) {
+        return message
+      }
+    })
+    
+    const sortedMessages = [{ message: [filteredMessages[0]] }];
+
+    for (let index = 1; index < filteredMessages.length; index++) {
+      
+      let currentMessage = filteredMessages[index]
+      let oldMessage = sortedMessages[sortedMessages.length-1].message[0]
+
+      let newId = currentMessage.user_id
+      let oldId = oldMessage.user_id
+
+      let newDate = new Date(currentMessage.created_at)
+      let oldDate = new Date(oldMessage.created_at)
+      if (newId === oldId && newDate.toDateString() === oldDate.toDateString()) {
+        sortedMessages[sortedMessages.length-1].message.push(currentMessage)
+      } else {
+        sortedMessages.push({ message:[currentMessage] })
+      }
+    }
+
+    debugger
 
     const messages = this.props.messages.map(message => {
       if (!message.id) {
--- frontend/components/message_index.jsx
+++ frontend/components/message_index.jsx
@@ -18,15 +18,35 @@ class MessageIndex extends React.PureComponent {
       }
     })
 
+    const sortedMessages = [{ message: [filteredMessages[0]] }];
+
+    for (let index = 1; index < filteredMessages.length; index++) {
+      
+      let currentMessage = filteredMessages[index]
+      let oldMessage = sortedMessages[sortedMessages.length-1].message[0]
+
+      let newId = currentMessage.user_id
+      let oldId = oldMessage.user_id
+
+      let newDate = new Date(currentMessage.created_at)
+      let oldDate = new Date(oldMessage.created_at)
+      if (newId === oldId && newDate.toDateString() === oldDate.toDateString()) {
+        sortedMessages[sortedMessages.length-1].message.push(currentMessage)
+      } else {
+        sortedMessages.push({ message:[currentMessage] })
+      }
+      
+    }
+
     const messages = this.props.messages.map(message => {
       return <MessageItem 
-        message={message}
+        message={message.message}
         channel={this.props.channel}
         deleteMessage={this.props.deleteMessage}
         currentUser={this.props.currentUser} 
         />
     })
-    
+
     return (
       <>
         {messages}
