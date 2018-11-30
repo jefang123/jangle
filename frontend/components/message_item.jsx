01d7a5e3e6d85cbb411694ed5f9f578bfc7d29c8
Jeff <jeffreyfang123@gmail.com>
2018-12-01 06:29:07 +0900

    Code out skeleton for messageitem

--- frontend/components/message_item.jsx
+++ frontend/components/message_item.jsx
@@ -15,6 +15,42 @@ class MessageItem extends React.PureComponent {
     this.props.deleteMessage(id);
   }
 
+          // <div key={message.id} className="message">
+          //   <div>
+          //     <img className="message-image" src={window.user_url}></img>
+          //     <div className="message-box">
+          //       <p >{user ? user.username : null} {timestamp}</p> 
+          //       <section>
+          //         <p className="message-body"> {message.body}</p>
+          //         {messageb}
+          //       </section>
+          //     </div>
+          //   </div>
+            
+          // </div>
+
+  // message = <section>
+  //             <p className="message-body"> {this.message.body} </p>
+  //             {messageb}
+  //           </section>
+
+  // message = this.props.message.map(mess => {
+  //   <section>
+  //     <p className="message-body"> {this.mess.body} </p>
+  //     {messageb}
+  //   </section>
+  // })
+
+  // <div key={message.id} className="message">
+  //   <div>
+  //     <img className="message-image" src={window.user_url}></img>
+  //     <div className="message-box">
+  //       <p> {user ? user.username : null} {timestamp}</p>
+  //       {message}
+  //     </div>
+  //   </div>
+  // </div>
+
   render () {
     let messageb;
     if (message.user_id === this.props.currentUser.id) {
