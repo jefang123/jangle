191e786baf5bdad35c63fcaa711022bf702e35a4
Jeff <jeffreyfang123@gmail.com>
2018-12-18 05:33:50 +0900

    Add update function to messages

--- frontend/components/message_item.jsx
+++ frontend/components/message_item.jsx
@@ -17,8 +17,7 @@ class MessageItem extends React.PureComponent {
     e.preventDefault();
   }
 
-  handleClick (e) {
-    e.preventDefault();
+  handleClick (id) {
     this.props.deleteMessage(id);
   }
 
@@ -47,26 +46,7 @@ class MessageItem extends React.PureComponent {
     }
   
     if (message.length === 1) {
-      let messageb;
-      if (message[0].user_id === this.props.currentUser.id) {
-        messageb = <p className="delete-message" onClick={()=>this.handleClick(message.id)}>Delete Message</p>
-      }
-
-      // <i className="fas fa-ellipsis-v" onclick={ ()=>this.handleEdit() } />
-      // <div className={showEdit}>
-      //   <p>Update Message</p>
-      //   <p>Delete Message</p>
-      // </div>
-      // if (this.state.edit) {
-      //   edit = <input placeholder={message[0].body} />
-      // }
-
-      // <p>escape to <span>cancel</span> enter to <span>save</span></p>
-      
-      let editText;
-      if (message[0].created_at !== message[0].updated_at) {
-        editText = <span>&nbsp;(edited)</span>
-      }
+      let editText, editable;
       let messageId = message[0].id;
 
       let handleEdit = this.props.handleEdit
@@ -74,52 +54,70 @@ class MessageItem extends React.PureComponent {
 
       const { edit, showMessageOptions } = this.props
       let showEdit = (messageId === showMessageOptions) ? "show-message-options" : "hidden-message-options"
-      
-      message2 = <section className="message-item" key={messageId} onClick={()=>this.handleClickOut()}>
-                <div className="message-body"> {message[0].body} {editText}</div>
+      if (message[0].created_at !== message[0].updated_at) {
+        editText = <span>&nbsp;(edited)</span>
+      }
 
-                <i className="fas fa-ellipsis-v" onClick={ ()=>handleMessageShow(messageId) } />
+      if (message[0].user_id === this.props.currentUser.id) {
+        editable = <> <i className="fas fa-ellipsis-v" onClick={ ()=>handleMessageShow(messageId) } />
                   <section className={showEdit}>
                     <p onClick={()=>handleEdit(messageId)}>Edit</p>
-                    <p onClick={()=>this.handleClick(e)}>Delete</p>
-                  </section>
-
-                  {/* <div className={showEdit} onClick={()=>this.handleEdit()}> 
-                    {messageb}
-                  </div> */}
-              </section>
+                    <p onClick={()=>this.handleClick(messageId)}>Delete</p>
+                  </section> </>
+      }
+      
+      message2 = <section className="message-item" key={messageId}>
+                  <div className="message-body"> {message[0].body} {editText}</div>
+                  {editable}                  
+                </section>
       
       
       if (edit === messageId) {
         message2 = <section className="message-item" key={message[0].id}>
-                    <MessageInput handleEdit={handleEdit} messageId={message[0].id} body={message[0].body} channel={this.props.channel} editId="edit"/>
+                    <MessageInput handleEdit={handleEdit} messageId={messageId} body={message[0].body} channel={this.props.channel} editId="edit"/>
                     <p className="message-edit">escape to <strong>cancel</strong> • enter to <strong>save</strong></p>
-        </section>
+                  </section>
       }
     
     } else {
-      const { edit, showMessageOptions} = this.props
+      const { edit, showMessageOptions, handleEdit, handleMessageShow } = this.props
+   
       message2 = this.props.message.map(mess => {
 
         let showEdit = mess.id === showMessageOptions ? "show-message-options" : "hidden-message-options"
-
-        let messagec;
-        if (mess.user_id === this.props.currentUser.id) {
-          messagec = <div className="delete-message" onClick={()=>this.handleClick(message.id)}>Delete Message</div>
+        let editText, editable;
+        if (mess.created_at !== mess.updated_at) {
+          editText = <span>&nbsp;(edited)</span>
         }
-        return (
-          <section className="message-item" key={mess.id} >
-            <div className="message-body"> {mess.body} </div>
-              <div className={showEdit} onClick={()=>this.handleEdit()}> 
-                {messagec}
-              </div>
-          </section>
-        )
+        if (mess.user_id === this.props.currentUser.id) {
+          editable = <> 
+                       <i className="fas fa-ellipsis-v" onClick={ ()=>handleMessageShow(mess.id) } />
+                        <section className={showEdit}>
+                          <p onClick={()=>handleEdit(mess.id)}>Edit</p>
+                          <p onClick={()=>this.handleClick(mess.id)}>Delete</p>
+                        </section> 
+                     </>
+        }
+        if (edit === mess.id) {
+          return (
+            <section className="message-item" key={mess.id} onClick={()=>this.handleClickOut()}>
+              <MessageInput handleEdit={handleEdit} messageId={mess.id} body={mess.body} channel={this.props.channel} editId="edit"/>
+              <p className="message-edit">escape to <strong>cancel</strong> • enter to <strong>save</strong></p>
+            </section>
+          )
+        } else {
+          return (
+            <section className="message-item" key={mess.id} onClick={()=>this.handleClickOut()}>
+              <div className="message-body"> {mess.body} {editText}</div>
+              {editable}
+            </section>
+          )
+        }
       })
    }
 
    return (
-    <div className="message">
+    <div className="message" onClick={()=>this.handleClickOut()}>
 
         <img className="message-image" src={window.user_url}></img>
         <div className="message-box">
