d2ed4f21233d2d6093c2571fe4baa92b417363b1
Jeff <jeffreyfang123@gmail.com>
2018-12-27 09:29:39 +0900

    Refactor

--- frontend/components/server_show.jsx
+++ frontend/components/server_show.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Redirect, Switch, matchPath} from 'react-router-dom'
+import { Redirect, Switch, matchPath} from 'react-router-dom'
 import ChannelShowContainer from './channel_show_container';
 import ChannelCreateContainer from './channel_create_container';
 import PrivateCreate from './channel_create_private_form';
@@ -199,54 +199,12 @@ class ServerShow extends React.PureComponent {
       parameter = 0;
     }
     
-    let klass;
-
     const { server, currentUser } = this.props;
     const channels = <ChannelIndex
                       channels={this.props.channels}
                       server={server}
                       currentUser={currentUser}
                       />
-
-    // const channels= this.props.channels.map( channel => {
-    //   if (channel.id === parameter) {
-    //     klass = "ch-selected"
-    //   } else {
-    //     klass = ""
-    //   }
-
-    //   let hash, channelName, deleteText, channelb;
-    //   if (server.private) {
-    //     hash = "@"
-    //     deleteText = "Delete Conversation";
-    //     if (channel.channel_name === currentUser.username) {
-    //       channelName = channel.channel_topic;
-    //     } else {
-    //       channelName = channel.channel_name;
-    //     }
-
-    //   } else {
-    //     hash = "#";
-    //     channelName = channel.channel_name;
-    //     deleteText = "Delete Channel";
-    //   }
-
-    //   if(server.creator_id === currentUser.id && this.props.channels.length > 1) {
-    //     channelb = 
-    //     <div className="delete-server" onClick={()=>{this.handleDeleteCh(channel.id)}}>
-    //       <i className="fas fa-cog"></i>
-    //     <div className="delete-hidden"><div className="arrow-left"></div>{deleteText}</div>
-    //     </div>
-    //   } 
-    //   return (
-    //     <Link key={channel.id} to={`/server/${server.id}/channel/${channel.id}`}>
-    //       <li className={klass}>
-    //         {hash}&nbsp;{channelName}
-    //         {channelb}
-    //       </li>
-    //     </Link>
-    //   )
-    // });
 
     const users = Object.values(this.props.users).map(user => {
       if (server.creator_id === user.id ) {
@@ -306,9 +264,7 @@ class ServerShow extends React.PureComponent {
               {modalshow}
               <br />
             </section>
-            {/* <ul> */}
               {channels}
-            {/* </ul> */}
             <div className="user-tab">
                 <div>
   
@@ -353,9 +309,7 @@ class ServerShow extends React.PureComponent {
               {modalshow}
               <br />
             </section>
-            {/* <ul> */}
               {channels}
-            {/* </ul> */}
             <div className="user-tab">
                 <div>
   
