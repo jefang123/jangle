80ed6a1de9d2e267c77a7946e4c15d4ac4edf41c
Jeff <jeffreyfang123@gmail.com>
2019-01-11 13:11:00 +0900

    Start on Search optimization for join server and private message

---
+++ frontend/components/search-bar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Search extends React.PureComponent {
+  constructor(props) {
+    super(props)
+  }
+
+  render () {
+    return null
+  }
+}
+
+export default Search;
