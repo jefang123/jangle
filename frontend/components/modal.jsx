a5c008c3f1c6f89f1f7bb94db821a252fd91b104
Jeff <jeffreyfang123@gmail.com>
2020-08-11 11:06:50 +0900

    Refactor Modal as functional component

--- frontend/components/modal.jsx
+++ frontend/components/modal.jsx
@@ -1,28 +1,17 @@
 import React from 'react';
 
-class Modal extends React.PureComponent {
-  constructor (props) {
-    super(props)
-    this.handleClose = this.props.handleClose
-    this.children = this.props.children
-  }
-
-  render () {
-    const showHideClassName = this.props.show ? 'modal display-block' : 'modal display-none';
-    return (
-      <div className={showHideClassName} >
-        <section className='modal-main'>
-          {this.children}
-          <button
-            className="modal-button"
-            onClick={this.handleClose}
-          >
-            Cancel
-          </button>
-        </section>
-      </div>
-    )
-  }
+const Modal = ({show, children, handleClose}) => {
+  const showHideClassName = show ? 'modal display-block' : 'modal display-none';
+  return (
+    <div className={showHideClassName}>
+      <section className='modal-main'>
+        {children}
+        <button className='modal-button' onClick={handleClose} >
+          Cancel
+        </button>
+      </section>
+    </div>
+  )
 }
 
 export default Modal;
